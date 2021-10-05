f01f8bcc58c8053f60a82d683b59d42ffc073a28
Jorge Castillo <jorge.castillo@idea23.mx>
2021-10-04 23:56:41 -0500

    feat(domain): Add SurchargeEntity.

--- lib/domain/entities/rate_entity.rb
+++ lib/domain/entities/rate_entity.rb
@@ -26,7 +26,7 @@ module Domain
       def initialize(
         id: nil,
         date: nil,
-        carrir:,
+        carrier:,
         carrier_service_name: nil,
         carrier_service_code: nil,
         carrier_logo_url: nil,
@@ -46,6 +46,46 @@ module Domain
         enviaya_service_name: nil,
         enviaya_service_code: nil
       )
+        raise TypeError, "id expected an Integer or NilClass but got: #{id.class}" unless id.is_a?(Integer) || id.is_a?(NilClass)
+        raise TypeError, "date expected a DateTime or NilClass but got: #{date.class}" unless data.is_a?(DateTime) || date.is_a?(NilClass)
+        raise TypeError, "carrier expected a String but got: #{carrier.class}" unless carrier.is_a?(String)
+        raise TypeError, "carrier_service_name expected a String or NilClass but got: #{carrier_service_name.class}" unless carrier_service_name.is_a?(String) || carrier_service_name.is_a?(NilClass)
+        raise TypeError, "carrier_service_code expected a String or NilClass but got: #{carrier_service_code.class}" unless carrier_service_code.is_a?(String) || carrier_service_code.is_a?(NilClass)
+        raise TypeError, "carrier_logo_url expected a String or NilClass but got: #{carrier_logo_url.class}" unless carrier_logo_url.is_a?(String) || carrier_logo_url.is_a?(NilClass)
+        raise TypeError, "carrier_logo_url_svg expected a String or NilClass but got: #{carrier_logo_url_svg.class}" unless carrier_logo_url_svg.is_a?(String) || carrier_logo_url_svg.is_a?(NilClass)
+        raise TypeError, "estimated_delivery expected a DateTime or NilClass but got: #{estimated_delivery.class}" unless estimated_delivery.is_a?(DateTime) || estimated_delivery.is_a?(NilClass)
+        raise TypeError, "net_shipping_amount expected a Float or NilClass but got: #{net_shipping_amount.class}" unless net_shipping_amount.is_a?(Float) || net_shipping_amount.is_a?(NilClass)
+        raise TypeError, "net_surcharges_amount expected a Float or NilClass but got: #{net_surcharges_amount.class}" unless net_surcharges_amount.is_a?(Float) || net_surcharges_amount.is_a?(NilClass)
+        raise TypeError, "net_total_amount expected a Float or NilClass but got: #{net_total_amount.class}" unless net_total_amount.is_a?(Float) || net_total_amount.is_a?(NilClass)
+        raise TypeError, "vat_amount expected a Float or NilClass but got: #{vat_amount.class}" unless vat_amount.is_a?(Float) || vat_amount.is_a?(NilClass)
+        raise TypeError, "vat_rate expected a Float or NilClass but got: #{vat_rate.class}" unless vat_rate.is_a?(Float) || vat_rate.is_a?(NilClass)
+        raise TypeError, "total_amount expected a Float or NilClass but got: #{total_amount.class}" unless total_amount.is_a?(Float) || total_amount.is_a?(NilClass)
+        raise TypeError, "public_total_amount expected a Float or NilClass but got: #{public_total_amount.class}" unless public_total_amount.is_a?(Float) || public_total_amount.is_a?(NilClass)
+        raise TypeError, "public_total_amount_currency expected a String or NilClass but got: #{public_total_amount_currency.class}" unless public_total_amount_currency.is_a?(String) || public_total_amount_currency.is_a?(NilClass)
+        raise TypeError, "currency expected a String or NilClass but got: #{currency.class}" unless currency.is_a?(String) || currency.is_a?(NilClass)
+
+
+        @id = id
+        @date = date
+        @carrier = carrier
+        @carrier_service_name = carrier_service_name
+        @carrier_service_code = carrier_service_code
+        @carrier_logo_url = carrier_logo_url
+        @carrier_logo_url_svg = carrier_logo_url_svg
+        @estimated_delivery = estimated_delivery
+        @net_shipping_amount = net_shipping_amount
+        @net_surcharges_amount = net_surcharges_amount
+        @net_total_amount = net_total_amount
+        @vat_amount = vat_amount
+        @vat_rate = vat_rate
+        @total_amount = total_amount
+        @public_total_amount = public_total_amount
+        @public_total_amount_currency = public_total_amount_currency
+        @currency = currency
+        @surcharges = surcharges
+        @service_terms = service_terms
+        @enviaya_service_name = enviaya_service_name
+        @enviaya_service_code = enviaya_service_code
       end
     end
   end
