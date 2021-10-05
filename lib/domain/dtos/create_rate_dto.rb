b3844235f3126a875c7dd0e70f6a36a0e782e24b
Jorge Castillo <jorge.castillo@idea23.mx>
2021-10-05 00:12:49 -0500

    feat(entities): Add constructor for CreateRateDto.

--- lib/domain/dtos/create_rate_dto.rb
+++ lib/domain/dtos/create_rate_dto.rb
@@ -1,3 +1,7 @@
+require_relative '../entities/direction_entity.rb'
+require_relative '../entities/shipment_entity.rb'
+require_relative '../entities/accounts_entity.rb'
+
 module Domain
   module Dtos
     class CreateRateDto
@@ -16,12 +20,19 @@ module Domain
         order_total_amount: nil,
         intelligent_filtering: nil
       )
-        self.origin_direction = origin_direction
-        self.destination_direction = destination_direction
-        self.shipment = shipment
-        self.accounts = accounts
-        self.order_total_amount = order_total_money
-        self.intelligent_filtering = intelligent_filtering
+        raise TypeError, "origin_direction expected a DirectionEntity or NilClass but got: #{origin_direction.class}" unless origin_direction.is_a?(::Domain::Entities::DirectionEntity) || origin_direction.is_a?(NilClass)
+        raise TypeError, "destination_direction expected a DirectionEntity or NilClass but got: #{destination_direction.class}" unless destination_direction.is_a?(::Domain::Entities::DirectionEntity) || destination_direction.is_a?(NilClass)
+        raise TypeError, "shipment expected a DirectionEntity but got: #{shipment.class}" unless shipment.is_a?(::Domain::Entities::ShipmentEntity)
+        raise TypeError, "accounts expected an AccountsEntity or NilClass but got: #{accounts.class}" unless accounts.is_a?(::Domain::Entities::AccountsEntity) || accounts.is_a?(NilClass)
+        raise TypeError, "order_total_amount expected an Integer or NilClass but got: #{order_total_amount.class}" unless order_total_amount.is_a?(Integer) || order_total_amount.is_a?(NilClass)
+        raise TypeError, "intelligent_filtering expected a Boolean or NilClass but got: #{intelligent_filtering.class}" unless intelligent_filtering.is_a?(Boolean) || intelligent_filtering.is_a?(NilClass)
+
+        @origin_direction = origin_direction
+        @destination_direction = destination_direction
+        @shipment = shipment
+        @accounts = accounts
+        @order_total_amount = order_total_money
+        @intelligent_filtering = intelligent_filtering
       end
     end
   end
